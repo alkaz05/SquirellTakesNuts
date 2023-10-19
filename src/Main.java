ba46c6b78e3cf1c45a726ab357d5ba17e3d41180
Aleksandr K <alkaz05@mail.ru>
2023-10-19 11:33:52 +0300

    name for Swuirrel

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Tree t = new Tree();
+
         //Nut[] mas = t.growNuts(7);
         Nut[] mas = t.growRandomNuts();
         System.out.println(Arrays.toString(mas));
-        Squirrel tirli = new Squirrel();
-        double w = tirli.nutCollecting(mas);
-        System.out.println("Белочка насобирала орехов весом "+w);
+        Squirrel tirli = new Squirrel("Тирли");
+        double w1 = tirli.nutCollecting(mas);
+        System.out.println("Белочка "+tirli.name+" насобирала орехов весом "+w1);
+
+        Nut[] mas2 = t.growRandomNuts();
+        System.out.println(Arrays.toString(mas2));
+        Squirrel dirli = new Squirrel("Дирли");
+        double w2 = dirli.nutCollecting(mas2);
+        System.out.println("Белочка "+dirli.name+" насобирала орехов весом "+w2);
 
     }
 }
--- src/Squirrel.java
+++ src/Squirrel.java
@@ -1,8 +1,14 @@
 public class Squirrel {
+    String name;
+
+    public Squirrel(String name) {
+        this.name = name;
+    }
+
     public double nutCollecting(Nut[] nutArr){
         double totalWeight = 0;
         for (int i = 0; i<nutArr.length; i++){
-            System.out.println("ура, еще орех!");
+            System.out.println("ура,"+name+" нашла еще орех!");
             totalWeight += nutArr[i].weight ;
         }
         //System.out.println("Белочка насобирала орехов весом "+totalWeight);
